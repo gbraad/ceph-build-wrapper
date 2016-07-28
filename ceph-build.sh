1c27faf94beeab3d31ad212a035a6472298a408b
Gerard Braad <me@gbraad.nl>
2016-07-28 14:02:19 +0000

    Use make-dist

--- ceph-build.sh
+++ ceph-build.sh
@@ -9,10 +9,9 @@ BUILDAREA=/root/rpmbuild
 mkdir -p ${SRCDIR}
 pushd ${SRCDIR}
 
-git clone git://github.com/ceph/ceph
+git clone git://github.com/ceph/ceph --depth 1 -b ${TAG}
 
 pushd ${SRCDIR}/ceph
-git checkout ${TAG}
 
 echo --START-IGNORE-WARNINGS
 yum install -y rpm-build rpmdevtools
@@ -29,40 +28,20 @@ if [ ! -e Makefile ]; then
     exit 3
 fi
 
-# Actually build the project
-
 # clear out any $@ potentially passed in
 set --
 
-# enable ccache if it is installed
-export CCACHE_DIR="$HOME/.ccache"
-if command -v ccache >/dev/null; then
-  if [ ! -e "$CCACHE_DIR" ]; then
-    echo "$0: have ccache but cache directory does not exist: $CCACHE_DIR" 1>&2
-  else
-    set -- CC='ccache gcc' CXX='ccache g++'
-  fi
-else
-  echo "$0: no ccache found, compiles will be slower." 1>&2
-fi
-
-#  Build Source tarball.  We do this after runing autogen/configure so that
-#  ceph.spec has the correct version number filled in.
-echo "**** Building Tarball ***"
-make dist-bzip2
+# Make tarball 
+./make-dist ${TAG}
 
 # Set up build area
-mkdir -p ${BUILDAREA}/SOURCES
-mkdir -p ${BUILDAREA}/SRPMS
-mkdir -p ${BUILDAREA}/SPECS
-mkdir -p ${BUILDAREA}/RPMS
-mkdir -p ${BUILDAREA}/BUILD
+rpmdev-setuptree
 cp -a ceph-*.tar.bz2 ${BUILDAREA}/SOURCES/.
 cp -a rpm/*.patch ${BUILDAREA}/SOURCES || true
 
 # Build RPMs
 BUILDAREA=`readlink -fn ${BUILDAREA}`   ### rpm wants absolute path
-rpmbuild -ba --define "_topdir ${BUILDAREA}" --define "_smp_mflags -j4" --define "dist .el7" ceph.spec
+rpmbuild -ba --define "_topdir ${BUILDAREA}" ceph.spec # --define "_smp_mflags -j4" --define "dist .el7" 
 
 popd
 
